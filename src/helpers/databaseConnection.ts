4a9d85bbc8081b14a84c17917c4324d654cda18c
NaisargiTrivedi <trivedi_naisargi@yahoo.in>
2022-08-05 12:39:07 +0530

    commit

--- src/helpers/databaseConnection.ts
+++ src/helpers/databaseConnection.ts
@@ -22,4 +22,6 @@ const sequelize: Sequelize = new Sequelize(process.env.DB_NAME as string, proces
     "host": "localhost"
 });
 
+console.log("s...........................",sequelize);
+
 export default sequelize;
